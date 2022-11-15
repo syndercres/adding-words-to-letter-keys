d99d4d272677280f3fa74ae9d0197d31148e8ff5
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-15 11:21:45 +0000

    implemented Psuedocode

--- src/kata/adding-words-to-letter-keys.ts
+++ src/kata/adding-words-to-letter-keys.ts
@@ -42,3 +42,41 @@ end
 
 return groupedWordsArr
 */
+
+function wordsToKeys(inputArr: string[]): {} {
+
+    interface objectOfWords {
+        [key: string]: string[]
+
+    }
+    
+    const keyArr: string[] = []
+
+    for (const word of inputArr) {
+        if(keyArr.includes(word[0]) === false) {
+            keyArr.push(word[0])
+        } 
+    }
+    const groupedWordsObject: objectOfWords = {}
+
+    for (const letter of keyArr) {
+        groupedWordsObject[letter] = collectWords(letter, inputArr)
+        console.log(groupedWordsObject)
+    }
+    
+    return groupedWordsObject
+}
+
+function collectWords(key: string, inputArr: string[]): string[] {
+    const groupedWordsArr: string[] = []
+
+    for(const word of inputArr) {
+        if((word[0] === key) && (groupedWordsArr.includes(word) !== true)) {
+            groupedWordsArr.push(word)
+        }
+    }
+
+    return groupedWordsArr
+}
+
+console.log(wordsToKeys(["shall", "i", "compare", "thee", "to", "a", "summer's"]));
